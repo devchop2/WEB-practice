ee540f05959a0929033df2c4b919e41bd3070bff
devchop <lhwlina1130@gmail.com>
2024-03-28 17:35:48 +0900

    add input event listener and add new item

--- todo-list/js/index.js
+++ todo-list/js/index.js
@@ -1,6 +1,43 @@
+//#region  variables
+
+const inputField = document.getElementById("todo-input");
+const listParent = document.getElementById("todo-parent");
+
+//#endregion
+
+//#region input event
 const onClickAdd = () => {
-  let addText = document.getElementById("todo-input").value;
+  let addText = inputField.value;
   if (!addText) return;
 
-  console.log(`add item:${addText}`);
+  createNewListItem(addText);
+  inputField.value = "";
 };
+
+const onInputKey = () => {
+  if (event.keyCode == 13) {
+    // if input enter key
+    onClickAdd();
+  }
+};
+
+//#endregion
+
+//#region manage list items
+const createNewListItem = (addText) => {
+  let li_tag = document.createElement("li");
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  let spanTag = document.createElement("span");
+  spanTag.textContent = addText;
+
+  li_tag.appendChild(checkbox);
+  li_tag.appendChild(spanTag);
+  li_tag.className = "sample";
+  console.log(li_tag);
+
+  // li_tag.textContent = addText;
+  listParent.append(li_tag);
+};
+//#endregion
