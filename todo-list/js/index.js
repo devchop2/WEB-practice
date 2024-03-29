95be1750a2d70cf2ef5113ae1914b0f62b566b8c
devchop <lhwlina1130@gmail.com>
2024-03-29 10:28:08 +0900

    implement remv func , localstorage

--- todo-list/js/index.js
+++ todo-list/js/index.js
@@ -3,14 +3,27 @@
 const inputField = document.getElementById("todo-input");
 const listParent = document.getElementById("todo-parent");
 
+const completeClassName = "span__complete";
+const saveFileName = "todo";
+
 //#endregion
+const initList = () => {
+  let json = localStorage.getItem(saveFileName);
+  if (!json) return;
+  let saveItems = JSON.parse(json);
+
+  for (let i = 0; i < saveItems.length; i++) {
+    createNewListItem(saveItems[i].name, saveItems[i].complete);
+  }
+};
 
 //#region input event
 const onClickAdd = () => {
   let addText = inputField.value;
   if (!addText) return;
 
-  createNewListItem(addText);
+  createNewListItem(addText, false);
+  saveData();
   inputField.value = "";
 };
 
@@ -24,25 +37,75 @@ const onInputKey = () => {
 //#endregion
 
 //#region manage list items
-const createNewListItem = (addText) => {
+const createNewListItem = (addText, completeFlag) => {
   let li_tag = document.createElement("li");
   let checkbox = document.createElement("input");
   let spanTag = document.createElement("span");
+  let btnTag = document.createElement("button");
 
   spanTag.textContent = addText;
-
+  spanTag.classList.add("todo__span");
   checkbox.type = "checkbox";
-  checkbox.className = "todo__item__checkbox";
+  checkbox.classList.add("todo__item__checkbox");
+
+  if (completeFlag) {
+    spanTag.classList.add(completeClassName);
+    checkbox.checked = true;
+  }
   checkbox.onclick = () => {
-    spanTag.classList.toggle("span__complete");
+    spanTag.classList.toggle(completeClassName);
+    saveData();
   };
+
+  btnTag.innerText = "Delete";
+  btnTag.classList.add("remove__button");
+  btnTag.addEventListener("click", () => {
+    li_tag.remove();
+    saveData();
+  });
 
   li_tag.appendChild(checkbox);
   li_tag.appendChild(spanTag);
+  li_tag.appendChild(btnTag);
+
   li_tag.className = "todo__list";
-  console.log(li_tag);
 
   // li_tag.textContent = addText;
   listParent.append(li_tag);
 };
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+
+  saveData();
+};
+
+//#endregion
+
+//#region  save
+
+const saveData = () => {
+  const liList = listParent.querySelectorAll("li");
+
+  let saveItems = [];
+  for (let i = 0; i < liList.length; i++) {
+    let spanTag = liList[i].querySelector("span");
+    if (!spanTag) continue;
+
+    let completeFlag = spanTag.classList.contains(completeClassName);
+    saveItems.push({ name: spanTag.textContent, complete: completeFlag });
+  }
+
+  let json = JSON.stringify(saveItems);
+  localStorage.setItem(saveFileName, json);
+
+  console.log(saveItems);
+};
+//#endregion
+
+//#region  Functions
+initList();
 //#endregion
